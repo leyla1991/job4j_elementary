92cf97b325a4d331febf1fdeea89fe08a41598a9
leyla1991 <atagleyla@mail.ru>
2022-04-12 18:39:07 +0300

    4.1. Разбор первой программы. [#159033]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,4 +1,8 @@
 package ru.job4j;
 
 public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Ivannykova Leyla");
+        System.out.println("27.11.1991");
+    }
 }
