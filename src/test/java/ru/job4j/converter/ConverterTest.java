ab1e6f64f21e189f0b9a15b2aeec3815d853be5b
leyla1991 <atagleyla@mail.ru>
2022-04-14 11:14:00 +0300

    1. Модульные тесты. [#159435]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,12 @@
+package ru.job4j.converter;
+
+import junit.framework.TestCase;
+
+public class ConverterTest extends TestCase {
+
+    public void testRubleToEuro() {
+    }
+
+    public void testRubleToDollar() {
+    }
+}
