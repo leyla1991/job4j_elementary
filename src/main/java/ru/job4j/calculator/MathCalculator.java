704e50931518cb1ddbca27712ad87eaea5836818
leyla1991 <atagleyla@mail.ru>
2022-04-14 12:06:05 +0300

    1.1. Что такое import [#504773]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -1,5 +1,7 @@
 package ru.job4j.calculator;
 
+import static ru.job4j.math.MathFunction.*;
+
 public class MathCalculator {
 
     public static double sumAndMultiply(double first, double second) {
@@ -7,7 +9,19 @@ public class MathCalculator {
                 + ru.job4j.math.MathFunction.multiply(first, second);
     }
 
+    public static double divAndDifference(double first, double second) {
+        return div(first, second)
+             + difference(first, second);
+    }
+
+    public static double sumAll(double first, double second) {
+        return div(first, second)
+                + difference(first, second)
+                + sum(first, second)
+                + multiply(first, second);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен " + sumAndMultiply(10, 20) + ", " + divAndDifference(40, 10) + ", " + sumAll(24, 8));
     }
 }
--- src/main/java/ru/job4j/math/MathFunction.java
+++ src/main/java/ru/job4j/math/MathFunction.java
@@ -8,4 +8,12 @@ public class MathFunction {
     public static double multiply(double first, double second) {
         return first * second;
     }
+
+    public static double difference(double first, double second) {
+        return first - second;
+    }
+
+    public static double div(double first, double second) {
+        return first / second;
+    }
 }
