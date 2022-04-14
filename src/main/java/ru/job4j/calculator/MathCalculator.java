dd84e0637659c3cfaff3ba0fea4930e9e6cb2431
leyla1991 <atagleyla@mail.ru>
2022-04-14 12:21:15 +0300

    1.1. Что такое import [#504773]

--- src/main/java/ru/job4j/calculator/MathCalculator.java
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -5,8 +5,8 @@ import static ru.job4j.math.MathFunction.*;
 public class MathCalculator {
 
     public static double sumAndMultiply(double first, double second) {
-        return ru.job4j.math.MathFunction.sum(first, second)
-                + ru.job4j.math.MathFunction.multiply(first, second);
+        return sum(first, second)
+                + multiply(first, second);
     }
 
     public static double divAndDifference(double first, double second) {
