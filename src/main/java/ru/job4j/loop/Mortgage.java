eef59cd1e9b26a6b86e2dd6b48d2ad4a4ed87191
leyla1991 <atagleyla@mail.ru>
2022-04-18 01:45:43 +0300

    5.7. Ипотека [#156315 #289901]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -4,7 +4,7 @@ public class Mortgage {
 
     public static int year(double amount, double salary, double percent) {
         int year = 0;
-        while (amount <= amount * (percent / 100) + amount) {
+        while (0 < amount) {
             year += 1;
             amount = amount * (percent / 100) + amount - salary;
         }
