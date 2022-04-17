e4d7cd237f23c3a1042fa334c36639dc914a29d7
leyla1991 <atagleyla@mail.ru>
2022-04-17 10:45:43 +0300

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public static int year(double amount, double salary, double percent) {
+        int year = 0;
+        while (amount <= amount * (percent / 100) + amount) {
+            year += 1;
+            amount = amount * (percent / 100) + amount - salary;
+        }
+        return year;
+    }
+}
