f719fc3f6290af1b0eade1f214368acc699ac12e
leyla1991 <atagleyla@mail.ru>
2022-04-18 19:33:02 +0300

    6.0.1. Объявление массива. [#3613]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -4,7 +4,7 @@ public class ArrayDefinition {
 
     public static void main(String[] args) {
         short[] ages = new short[10];
-        String[] surnames = new String[100500];
+        String[] surnames = new String[10500];
         float[] prices = new float[40];
     }
 
